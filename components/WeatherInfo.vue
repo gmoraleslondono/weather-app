a480a052201c01f9bba41d5fd9043589aad0becf
Gloria Morales <gmoraleslondono@gmail.com>
2019-12-14 15:01:55 +0100

    Add eatherInfo component

---
+++ components/WeatherInfo.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h3>{{ cityName}}</h3>
+    <p>Temperature:<strong>{{ currentTemp }}</strong></p>
+    <p>
+      <span v-if="hasMinTemp">min:<strong>{{ minTemp }}</strong></span>
+      <span>max:
+        <strong v-if="maxTemp !== ''">{{ maxTemp }}</strong>
+        <strong v-else> -- </strong>
+      </span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherInfo',
+  props: {
+    cityName: {
+      type: String,
+      required: true
+    },
+    currentTemp: {
+      type: Number,
+      required: true
+    },
+    minTemp: {
+      //way one
+      type: [Number, String],
+      default: '--', //if we dont have answer
+      validator: function (value) {
+        if(typeof value !== Number) {
+          return false;
+        }
+      }
+    },
+    maxTemp: {
+      //way two
+      type: [Number, String],
+      default: '', //if we dont have answer
+    }
+  },
+  computed: {
+    hasMinTemp() {
+      return typeof value === Number
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
